1ad5d46ef58c26f2624d428fcc96cf4e86eb3cdc
unknown <hnamechAlberto Cervantes>
2022-09-09 13:50:45 +0200

    Add capture windows and save image

---
+++ MLPoker.App/MLPoker.App/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MLPoker.App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btnWindow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(1082, 757);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(186, 95);
+            this.btnCapture.TabIndex = 0;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(12, 12);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(1064, 840);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 1;
+            this.pbImage.TabStop = false;
+            // 
+            // btnWindow
+            // 
+            this.btnWindow.Location = new System.Drawing.Point(1082, 662);
+            this.btnWindow.Name = "btnWindow";
+            this.btnWindow.Size = new System.Drawing.Size(186, 89);
+            this.btnWindow.TabIndex = 2;
+            this.btnWindow.Text = "Window";
+            this.btnWindow.UseVisualStyleBackColor = true;
+            this.btnWindow.Click += new System.EventHandler(this.btnWindow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 859);
+            this.Controls.Add(this.btnWindow);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.btnCapture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnCapture;
+        private PictureBox pbImage;
+        private Button btnWindow;
+    }
+}
---
+++ MLPoker.App/MLPoker.App/Form1.cs
@@ -0,0 +1,30 @@
+using MLPoker.App.UseCases;
+
+namespace MLPoker.App
+{
+    public partial class Form1 : Form
+    {
+        private readonly GetWindowScreenUseCase _useCaseScreen;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _useCaseScreen = new GetWindowScreenUseCase();
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            var response = _useCaseScreen.Execute(new GetWindowScreenUseCaseRequest { Path = @"C:\Code\MachineLearning\MLPoker\MLPoker.App\MLPoker.App\DataSet\Imagen_" + DateTime.Now.Ticks + ".jpg" });
+
+            Thread.Sleep(100);
+            pbImage.Image = Image.FromFile(response.Position);
+
+        }
+
+        private void btnWindow_Click(object sender, EventArgs e)
+        {
+            Thread.Sleep(2000);
+            _useCaseScreen.Window();
+        }
+    }
+}
